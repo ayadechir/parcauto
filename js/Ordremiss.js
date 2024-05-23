38178e5f4ba4d885d8b3d390d06de6182cb4cfb3
thenewinfo <133818395+thenewinfo@users.noreply.github.com>
2024-05-23 21:11:20 +0100

    Update Ordremiss.js

--- js/Ordremiss.js
+++ js/Ordremiss.js
@@ -1,36 +1,53 @@
-
-
-// Fonction pour récupérer les informations (nom, prénom) associées au matricule sélectionné
-function recupererInfos() {
-    // Récupérer le matricule sélectionné
-    var matricule = document.getElementById('matricule').value;
-
-    // Effectuer une requête AJAX vers le serveur pour récupérer les informations associées au matricule
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                // Traitement de la réponse JSON
-                var reponse = JSON.parse(xhr.responseText);
-                // Mettre à jour les champs de nom et prénom avec les données récupérées
-                document.getElementById('nom_prenom').value = reponse.nom + ' ' + reponse.prenom;
-            } else {
-                // Gérer les erreurs
-                console.error('Erreur lors de la récupération des informations.');
-            }
-        }
-    };
-   xhr.open('POST', 'parcauto/php/Ordremiss.php'); 
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send('matricule=' + matricule);
-}
-
-// Ajouter un événement onchange au champ de sélection (matricule)
-document.getElementById('matricule').addEventListener('change', recupererInfos);
-
-
-
-
 function imprimerPage() {
     window.print();
 }
+$(document).ready(function() {
+    $('#creer').click(function() {
+        $('#form-container').show();
+        $('#tabular--wrapper').hide();
+    });
+});
+$(document).ready(function() {
+    $('#imprime').click(function() {
+        $('#tabular--wrapper').show();
+        $('#form-container').hide();
+    });
+});
+$(document).ready(function(){
+    $(".print").click(function(){
+      $("#myModal").fadeIn();
+    });
+    $(".close").click(function(){
+      $("#myModal").fadeOut();
+    });
+  });
+
+function showOr(id_or,nom_prenom,matricule,adress_admin,emplacement,raison,date_dep,date_ret,matricule_v) {
+    $("#myModal").css("display", "block");
+    $(".id_or").val(id_or);
+    $("#nom_prenom_").val(nom_prenom);
+    $("#matricule_").val(matricule);
+    $("#adress_admin_").val(adress_admin);
+    $("#emplacement_").val(emplacement);
+    $("#raison_").val(raison);
+    $("#date_dep_").val(date_dep);
+    $("#date_ret_").val(date_ret);
+    $("#matricule_v_").val(matricule_v);
+  }
+
+
+
+function formatDate(date) {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
+function setDate() {
+  const today = new Date();
+  const formattedDate = formatDate(today);
+  document.getElementById('dateInput').value = formattedDate;
+}
+
+document.addEventListener('DOMContentLoaded', setDate);
